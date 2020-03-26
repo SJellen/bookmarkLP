ae102ce4010ba80e2b2584eba30b7f7bfc7d9eb6
scott jellen <scottjellen@scotts-MBP.fios-router.home>
2020-03-26 16:22:17 -0400

    layed out tab change

--- script.js
+++ script.js
@@ -12,3 +12,44 @@ function toggleMenu() {
 }
 
 hamburger.addEventListener('click', toggleMenu)
+
+
+const tab1 = document.querySelector('#tab1')
+const tab2 = document.querySelector('#tab2')
+const tab3 = document.querySelector('#tab3')
+
+const tab1H3 = "Bookmark in one click"
+const tab2H3 = "Intelligent search"
+const tab3H3 = "Share your bookmarks"
+
+const tab1P = "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites."
+
+const tab2P = "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks."
+
+const tab3P = "Easily share your bookmarks and collections with others. Create a sharable link that you can send at the click of a button."
+
+tab1.addEventListener('click', changeTab1);
+tab2.addEventListener('click', changeTab2);
+tab3.addEventListener('click', changeTab3);
+
+
+function changeTab1() {
+    document.querySelector('.tab-image').src = "./images/illustration-features-tab-1.svg"
+    document.getElementById('h3-text').innerHTML = tab1H3
+    document.getElementById('p-text').innerHTML = tab1P
+}
+
+
+
+function changeTab2() {
+    document.querySelector('.tab-image').src = "./images/illustration-features-tab-2.svg"
+    document.getElementById('h3-text').innerHTML = tab2H3
+    document.getElementById('p-text').innerHTML = tab2P
+}
+
+function changeTab3() {
+    document.querySelector('.tab-image').src = "./images/illustration-features-tab-3.svg"
+    document.getElementById('h3-text').innerHTML = tab3H3
+    document.getElementById('p-text').innerHTML = tab3P
+}
+
